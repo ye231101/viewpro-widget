4b152d6a8d82601ab6cac7c879763d8b1b488236
ye231101 <ye231101@gmail.com>
2026-01-21 13:42:56 -0500

    chore: add the contants

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:8000';
+export const BASE_URL = SERVER_URL + '/api';
+export const AVATAR_URL = SERVER_URL + '/public/avatars/';
+
+export const LIVEKIT_URL = process.env.REACT_APP_LIVEKIT_URL || '';
+
+export const username = 'Client';
