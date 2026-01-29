4176efe58703582e2343309625d0830c9d5d38bf
ye231101 <ye231101@gmail.com>
2026-01-29 02:07:51 -0500

    fix: update the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   resolve: {
